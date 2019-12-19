68384d9c33ff87283823e67ef635a30c0a669474
rargelaguet <ricard.argelaguet@gmail.com>
2019-12-19 13:51:58 +0100

    add pseudobulk scripts

--- met/pseudobulk/pseudobulk_met.R
+++ met/pseudobulk/pseudobulk_met.R
@@ -31,7 +31,7 @@ io <- list()
 if (grepl("ricard",Sys.info()['nodename'])) {
   io$basedir <- "/Users/ricard/data/gastrulation"
 } else {
-  io$basedir <- "/hps/nobackup2/research/stegle/users/ricard"
+  io$basedir <- "/hps/nobackup2/research/stegle/users/ricard/gastrulation"
 }
 io$data <- paste0(io$basedir,"/met/cpg_level")
 io$metadata <- paste0(io$basedir,"/sample_metadata.txt")
